4b0ce343475110b36546ec66a2cf06f04d7ee4bb
scarperez7 <95876497+scarperez7@users.noreply.github.com>
2022-04-26 21:44:26 -0400

    installed bootstrap/minor appearance changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <footer className="fixed-bottom text-center mt-2">
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/scarperez7"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Open Source code
         </a>
-      </header>
+        {""} by Scarlett Perez
+      </footer>
     </div>
   );
 }
